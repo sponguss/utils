25db25a01b73ef7a8a6d05ae3af3d3588d1302ba
spongus <112719635+sponguss@users.noreply.github.com>
2022-09-16 23:01:41 -0300

    Create identifyexecutor.lua

---
+++ identifyexecutor.lua
@@ -0,0 +1,9 @@
+return (
+    identifyexecutor and identifyexecutor() or
+    secure_load and "Sentinel" or
+    is_sirhurt_closure and "Sirhurt" or
+    pebc_execute and "ProtoSmasher" or
+    (syn and syn.protect_gui) and "Synapse X" or
+    (SendLuaCScript) and "WeAreDevs" or
+    "Unknown"
+)
